dec85e0f7c7349e2d88db8e092565d2d9450a91f
Simon <simon.tan09@gmail.com>
2022-09-26 15:20:39 -0700

    Add migrate and model tables

--- backend/db/seeders/20220922194551-demo-user.js
+++ backend/db/seeders/20220922194551-demo-user.js
@@ -23,17 +23,45 @@ module.exports = {
       {
         email: 'user1@user.io',
         username: 'FakeUser1',
-        firstName:'user1',
-        lastName:'user',
+        firstName:'Jame',
+        lastName:'John',
         hashedPassword: bcrypt.hashSync('password2')
       },
       {
         email: 'user2@user.io',
         username: 'FakeUser2',
-        firstName:'user2',
-        lastName:'user',
+        firstName:'Cindy',
+        lastName:'David',
         hashedPassword: bcrypt.hashSync('password3')
-      }
+      },
+      {
+        email: 'user3@user.io',
+        username: 'FakeUser3',
+        firstName:'Evan',
+        lastName:'William',
+        hashedPassword: bcrypt.hashSync('password4')
+      },
+      {
+        email: 'user4@user.io',
+        username: 'FakeUser4',
+        firstName:'Ben',
+        lastName:'Henry',
+        hashedPassword: bcrypt.hashSync('password5')
+      },
+      {
+        email: 'user5@user.io',
+        username: 'FakeUser5',
+        firstName:'Emma',
+        lastName:'West',
+        hashedPassword: bcrypt.hashSync('password6')
+      },
+      {
+        email: 'user6@user.io',
+        username: 'FakeUser6',
+        firstName:'Eve',
+        lastName:'East',
+        hashedPassword: bcrypt.hashSync('password7')
+      },
     ], {});
   },
 
