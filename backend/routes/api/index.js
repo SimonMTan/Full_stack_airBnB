cae11d9537f4c3d3caa6591ec6d518c9ff216ff9
Simon <simon.tan09@gmail.com>
2022-09-27 14:29:33 -0700

    finish 2nd and 3rd endpoint

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -5,9 +5,9 @@ const spotsRouter = require('./spots.js')
 // const { restoreUser } = require('../../utils/auth.js');
 // const { setTokenCookie } = require('../../utils/auth.js');
 // const { User } = require('../../db/models');
-// const { restoreUser } = require("../../utils/auth.js");
+const { restoreUser } = require("../../utils/auth.js");
 
-// router.use(restoreUser);
+router.use(restoreUser);
 router.use('/session', sessionRouter);
 router.use('/users', usersRouter);
 router.use('/spots', spotsRouter);
--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -1,13 +1,14 @@
 const express = require('express')
 const router = express.Router();
-const { Spot,Review,SpotImage,sequelize } = require('../../db/models');
+const { Spot,Review,SpotImage,sequelize,User } = require('../../db/models');
+const user = require('../../db/models/user');
+const { setTokenCookie, restoreUser, requireAuth, } = require('../../utils/auth');
 
 // Get all Spots
 router.get('/', async (req, res, next) => {
 
     let { size, page } = req.query
     let pagination = {}
-    let emptyArray = []
 
     if (!page) {page = 1}
     if (!size) {size = 20}
@@ -15,17 +16,18 @@ router.get('/', async (req, res, next) => {
     size = parseInt(size);
 
     if (page >= 1 && size >= 1) {
-      pagination.limit = size
-      pagination.offset = size * (page - 1)
+        pagination.limit = size
+        pagination.offset = size * (page - 1)
 
     }
 
     const spots = await Spot.findAll({
-      ...pagination
+        ...pagination
     })
 
+    let emptyArray = []
     for (let spot of spots) {
-      const ratings = await Review.findAll({
+      const review = await Review.findAll({
         raw: true,
         where: {
           spotId: spot.id
@@ -35,7 +37,7 @@ router.get('/', async (req, res, next) => {
         ],
       })
 
-      let imageUrl = await SpotImage.findOne( {
+      let sptImg = await SpotImage.findAll( {
         raw: true,
         where: {
             spotId: spot.id,
@@ -45,8 +47,8 @@ router.get('/', async (req, res, next) => {
       })
 
       let spotJson = spot.toJSON()
-      spotJson.avgRating = Number(ratings[0].avg).toFixed(1),
-      spotJson.previewImage = imageUrl.url
+      spotJson.avgRating = Number(review[0].avg).toFixed(1)
+      spotJson.previewImage = sptImg[0].url
 
       emptyArray.push(spotJson)
     }
@@ -58,9 +60,88 @@ router.get('/', async (req, res, next) => {
     })
   });
 
+//   Get all Spots owned by the Current User
+router.get('/current', requireAuth, async (req, res, next) => {
+    const spots = await Spot.findAll({
+        raw:true,
+        where:{
+            ownerId:req.user.id
+        }
+    })
+
+    let emptyArray = []
+    for (let spot of spots) {
+      const review = await Review.findAll({
+        raw: true,
+        where: {
+          spotId: spot.id
+        },
+        attributes: [
+          [sequelize.fn("AVG", sequelize.col('stars')), "avg"],
+        ],
+      })
+
+      let sptImg = await SpotImage.findAll( {
+        raw: true,
+        where: {
+            spotId: spot.id,
+            preview: true
+            },
+        attributes: ['url']
+      })
+
+      spot.avgRating = Number(review[0].avg).toFixed(1)
+      spot.previewImage = sptImg[0].url
+
+      emptyArray.push(spot)
+    }
+    res.json({
+        Spots: emptyArray })
+
+})
+
 // Get details of a Spot from an id
+router.get('/:spotId',async(req,res,next) =>{
+    const {spotId} = req.params
+    const spot = await Spot.findByPk(spotId,{raw:true})
+    if(!spot){
+        res.status(404).json({
+            "message": "Spot couldn't be found",
+            "statusCode": 404
+          })
+    }
+    const review = await Review.findAll({
+        raw: true,
+        where: {
+          spotId: spotId
+        },
+        attributes: [
+          [sequelize.fn("AVG", sequelize.col('stars')), "avg"],
+          [sequelize.fn("COUNT", sequelize.col('stars')), "count"],
+        ],
+      })
 
+      let sptImgs = await SpotImage.findAll( {
+        raw: true,
+        where: {
+            spotId: spot.id,
+            preview: true
+            },
+        attributes: ['id','url','preview']
+      })
 
+      let owner = await User.findByPk(spot.ownerId,{attributes:['id','firstName','lastName']})
 
+    spot.numReviews = Number(review[0].count)
+    spot.avgStarRating = Number(review[0].avg).toFixed(1)
+    spot.SpotImages = sptImgs
+    spot.Owner = owner
+
+        res.json({
+            Spots: spot })
+
+})
+
+// Create a Spot
 
 module.exports = router;
