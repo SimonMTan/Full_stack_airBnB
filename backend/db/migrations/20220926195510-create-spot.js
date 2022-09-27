8165bd785a7d3e155d8536c444ad5263b1fda10d
Simon <simon.tan09@gmail.com>
2022-09-26 17:43:49 -0700

    debug #1 on heroku sportId_foreignkeyerror

--- backend/db/migrations/20220926195510-create-spot.js
+++ backend/db/migrations/20220926195510-create-spot.js
@@ -11,8 +11,8 @@ module.exports = {
       ownerId: {
         type: Sequelize.INTEGER,
         allowNull:false,
-        reference: {model:'Users',key:'id'},
-        onDelete:"CASCADE"
+        reference: {model:'Users'},
+        onDelete: 'CASCADE'
       },
       address: {
         type: Sequelize.STRING
--- backend/db/models/spot.js
+++ backend/db/models/spot.js
@@ -11,31 +11,34 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
-      Spot.belongsTo(models.User,{
-        foreignKey:'ownerId'
+      Spot.belongsTo(models.User, {
+        foreignKey: 'ownerId'
       }),
-      Spot.hasMany(models.Booking,{
-        foreignKey:'spotId'
-      }),
-      Spot.hasMany(models.Review,{
-        foreignKey:'spotId'
-      }),
-      Spot.hasMany(models.SpotImage,{
-        foreignKey:'spotId'
-      })
+        Spot.hasMany(models.Booking, {
+          foreignKey: 'spotId'
+        }),
+        Spot.hasMany(models.Review, {
+          foreignKey: 'spotId'
+        }),
+        Spot.hasMany(models.SpotImage, {
+          foreignKey: 'spotId'
+        })
     }
   }
   Spot.init({
-    ownerId: DataTypes.STRING,
-    address: DataTypes.STRING,
-    city: DataTypes.STRING,
-    state: DataTypes.STRING,
-    country: DataTypes.STRING,
-    lat: DataTypes.INTEGER,
-    lng: DataTypes.INTEGER,
-    name: DataTypes.STRING,
-    description: DataTypes.STRING,
-    price: DataTypes.INTEGER
+    ownerId: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    },
+    address: { type: DataTypes.STRING },
+    city: { type: DataTypes.STRING },
+    state: { type: DataTypes.STRING },
+    country: { type: DataTypes.STRING },
+    lat: { type: DataTypes.FLOAT },
+    lng: { type: DataTypes.FLOAT },
+    name: { type: DataTypes.STRING },
+    description: { type: DataTypes.STRING },
+    price: { type: DataTypes.FLOAT }
   }, {
     sequelize,
     modelName: 'Spot',
