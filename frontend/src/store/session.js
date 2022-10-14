6372199f1e87b0d29bdb523b4583fe79654e2c1a
Simon <simon.tan09@gmail.com>
2022-10-13 22:55:47 -0700

    phase 1 done part 1

--- frontend/src/store/session.js
+++ frontend/src/store/session.js
@@ -31,13 +31,12 @@ export const login = (user) => async (dispatch) => {
 };
 
 export const restoreUser = () => async dispatch => {
-    const response = await csrfFetch('/api/session');
-    const data = await response.json();
-    dispatch(setUser(data.user));
-    return response;
-  };
+  const response = await csrfFetch('/api/session');
+  const data = await response.json();
+  dispatch(setUser(data.user));
+  return response;
+};
 
-  
 const initialState = { user: null };
 
 const sessionReducer = (state = initialState, action) => {
