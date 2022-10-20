cfdd2658d34561358c35162cc6145f27a4cb4700
Simon <simon.tan09@gmail.com>
2022-10-20 00:01:11 -0700

    review done except review create

--- frontend/src/components/Review/Createreview_spot/index.js
+++ frontend/src/components/Review/Createreview_spot/index.js
@@ -1,5 +1,11 @@
-const Createreview = () =>{
+import  { useState } from "react";
 
+const Createreview = () =>{
+    const [review,setReview] = useState('')
+    const [stars,setStars] = useState(0)
+
+    
+    return null
 }
 
 export default Createreview
