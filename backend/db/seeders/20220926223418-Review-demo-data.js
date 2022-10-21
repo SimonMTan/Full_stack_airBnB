e77712e18c56e999a357abde5209e0f5c531bb4c
Simon <simon.tan09@gmail.com>
2022-10-21 12:32:43 -0700

    updates css

--- backend/db/seeders/20220926223418-Review-demo-data.js
+++ backend/db/seeders/20220926223418-Review-demo-data.js
@@ -14,36 +14,36 @@ module.exports = {
     await queryInterface.bulkInsert('Reviews', [
       {
         spotId: 1,
-        userId: 1,
+        userId: 5,
         review: "This is Great!",
         stars: 5
       }, {
         spotId: 2,
-        userId: 2,
+        userId: 3,
         review: "This is Good!",
         stars: 5
       },
       {
         spotId: 3,
-        userId: 3,
+        userId: 4,
         review: "This is Awesome!",
         stars: 5
       },
       {
         spotId: 4,
-        userId: 4,
+        userId: 5,
         review: "This is Fantastic!",
         stars: 4
       },
       {
         spotId: 5,
-        userId: 5,
+        userId: 6,
         review: "This is unique and beautiful!",
         stars: 4
       },
       {
         spotId: 6,
-        userId: 6,
+        userId: 3,
         review: "This place is so beautiful, will book again!",
         stars: 5
       },
