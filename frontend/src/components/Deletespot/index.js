d0a70dd7de46b9a9d5aa5e8a771d81dda024d1b9
Simon <simon.tan09@gmail.com>
2022-10-24 07:07:55 -0700

    last bug

--- frontend/src/components/Deletespot/index.js
+++ frontend/src/components/Deletespot/index.js
@@ -2,7 +2,7 @@ import { useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { NavLink, useParams } from "react-router-dom";
 import { deletespot } from "../../store/spots";
-
+import './delete.css'
 const Deletespot = () =>{
     const dispatch = useDispatch()
     const {spotId} = useParams()
@@ -16,7 +16,7 @@ const Deletespot = () =>{
         <h1>Successfully Deleted</h1>
         <span >
             <NavLink to={'/account'}>
-                <u>return to Account</u>
+                <div className="returntoaccount"><u>return to Account</u></div>
             </NavLink>
         </span>
     </div>)
